91255c3a450d3f89e471d20c6077bcef269fefe2
Michal Sierpien <michal.sierpien@gmail.com>
2019-12-05 21:05:12 +0100

    margin top nawbar

--- src/views/Media.vue
+++ src/views/Media.vue
@@ -4,7 +4,7 @@
             <nav class="">
                 <ul class="navbar">
                    <li class="navbar__item"><a class="navbar__link" href="/form">?</a></li>
-                   <li class="navbar__item"><a class="navbar__link" href="#"><img src="../assets/img/youtube.png" /></a></li>
+                   <li class="navbar__item"><a class="navbar__link" href="https://www.youtube.com/channel/UCOB2dfn9MXlBfK51fnEJlhw"><img src="../assets/img/youtube.png" /></a></li>
                 </ul>
             </nav>
         </header>
