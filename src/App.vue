d428e83f38b44f285fe36996fc8becba05d0897c
Michal Sierpien <michal.sierpien@gmail.com>
2019-12-05 23:03:27 +0100

    create form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      </div>
+
     <router-view/>
-  </div>
+
 </template>
 
 <style lang="scss">
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
+
 
   body {
     font-family: 'Fjalla One', sans-serif;
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
-        <header>
-            test
+        <header class="ph-hero">
+            <h1><span>P</span>ozdro<span>h</span>ejt</h1>
+            <p>A ty jakie masz <strong>Ph?</strong></p>
         </header>
         <main>
+            <div class="ph-wrapper">
+                <ol>
+                    <li class="li">Osądzasz innych</li>
+                    <li class="li">Oceniasz innych</li>
+                    <li class="li">Krytykujesz</li>
+                    <li class="li">Analizujesz opinie innych</li>
+                    <li class="li">Bierzesz wszystko do siebie</li>
+                </ol>
+<!--                <button class="btn-ph">Yep!</button>-->
+
+            </div>
 
         </main>
     </div>
